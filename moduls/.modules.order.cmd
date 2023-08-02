aeee3faa52cbb0b601b514465130fb35456e25d1
alex <alexsychev27@gmail.com>
2023-08-02 14:21:21 +0300

    step 2 of kernel exercise

---
+++ moduls/.modules.order.cmd
@@ -0,0 +1,1 @@
+cmd_/home/ales/Documents/moduls/modules.order := {   echo /home/ales/Documents/moduls/hello-check-point-module.ko; :; } | awk '!x[$$0]++' - > /home/ales/Documents/moduls/modules.order
